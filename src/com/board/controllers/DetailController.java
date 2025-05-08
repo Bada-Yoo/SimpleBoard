0538d4878d8ff2142702997dcc39f3b4bc349f49
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-05-08 09:08:45 +0900

    :sparkles: DetailController 생성

---
+++ src/com/board/controllers/DetailController.java
@@ -0,0 +1,14 @@
+package com.board.controllers;
+
+import com.board.front.ActivateControllerInterface;
+
+import java.util.Scanner;
+
+public class DetailController implements ActivateControllerInterface {
+    Scanner sc = new Scanner(System.in);
+
+    @Override
+    public void execute() {
+
+    }
+}
