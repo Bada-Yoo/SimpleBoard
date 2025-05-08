f015fc732966e6818b95e45278097e8e42ad71b6
josangho99 <josangho99@gmail.com>
2025-05-08 09:42:08 +0900

    add: DAO insert(write) 및 makeBoard()

--- src/com/board/entity/BoardDAO.java
+++ src/com/board/entity/BoardDAO.java
@@ -1,5 +1,51 @@
 package com.board.entity;
 
-public class BoardDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.board.front.DBUtil;
 
+public class BoardDAO {
+	
+	//insert
+	public static int insertBoard(String title, String content, String writer) {
+		int result = 0;
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement st = null;
+		String sql = """
+				insert into cart(
+					ID,
+					TITLE,
+					CONTENT,
+					WRITER,
+					CREATEDDATE,
+				)values(board_seq.nextval,?,?,?,sysdate)
+				""";
+		
+		try {
+			st = conn.prepareStatement(sql);
+			st.setString(1, title);
+			st.setString(2, content);
+			st.setString(3, writer);
+			result = st.executeUpdate();
+		} catch (SQLException e) {
+			
+		} finally {
+			DBUtil.dbDisconnect(conn, st, null);
+		}
+		return result;
+	}
+	
+	//make
+	private BoardDTO makeBoard(ResultSet rs) throws SQLException {
+		BoardDTO board = BoardDTO.builder()
+				.id(rs.getInt("ID"))
+				.title(rs.getString("TITLE"))
+				.content(rs.getString("CONTENT"))
+				.writer(rs.getString("WRITER"))
+				.createdDate(rs.getTimestamp("CREATEDDATE"))
+				.build();
+		return board;
+	}
 }
